9e4d739eda8be1a7a6856e5ff9e13f0ea597f2b3
failsafe89 <failsafe89@gmail.com>
2020-01-05 11:49:21 +0000

    repo refactor and addition of licence boilerplate

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pydish',
+    version=0.1,
+    scripts=[],
+    author='failsafe89',
+    author_email='',
+    description='No Dependency, cross platform, Display/Input driver shim for python3 using OpenGL (via websockets and webgl)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/failsafe89/pydish',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache 2.0 License",
+        "Operating System :: OS Independent"
+    ],
+)
